f1c80161f96360cc73254992783f9cb759f8ba57
gandxo-tech <gbaguidiexauce@gmail.com>
2025-08-29 10:58:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-.
+const CACHE_NAME = "sis-cache-v3"; // change si tu veux forcer maj
+const urlsToCache = [
+  "index.html",
+  "envoyer.html",
+  "voir.html",
+  "avis.html",
+  "conditions.html",
+  "politiques.html",
+  "propos.html",
+  "icon/icon-192.png",
+  "icon/icon-512.png"
+];
+
+// INSTALL : mise en cache direct
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting(); // active direct
+});
+
+// ACTIVATE : nettoie anciens caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// FETCH : réponse cache d’abord, sinon réseau
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
